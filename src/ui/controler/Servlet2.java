2eab524df903c50d4e276e7bbb4da9a9bbc7aacc
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2020-10-25 15:50:10 +0100

    Initial commit

---
+++ src/ui/controler/Servlet2.java
@@ -0,0 +1,106 @@
+package ui.controler;
+import domain.database.DatabankenPlayers;
+import domain.model.Player;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.util.ArrayList;
+
+@WebServlet("/servlet")
+public class Servlet2 extends HttpServlet {
+
+    DatabankenPlayers databankenPlayers = new DatabankenPlayers();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        giverOfTasks(request,response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        giverOfTasks(request,response);
+    }
+
+    private void giverOfTasks(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+        String task = "home";
+        if (request.getParameter("task") != null){
+            task = request.getParameter("task");
+        }
+
+        String a = "";
+        switch (task){
+            case "home":
+                a = home(request,response);
+                break;
+            case "overview":
+                a = overview(request,response);
+                break;
+            case "delete":
+                a = delete(request,response);
+                break;
+            case "add":
+                a = add(request,response);
+                break;
+            case "find":
+                a = find(request,response);
+        }
+        request.getRequestDispatcher(a).forward(request, response);
+    }
+
+    private String home (HttpServletRequest request, HttpServletResponse response){
+        return "index.html";
+    }
+
+    private String overview (HttpServletRequest request, HttpServletResponse response){
+        request.setAttribute("p1","null");
+        request.setAttribute("p2","null");
+        request.setAttribute("db",databankenPlayers.getSpelers());
+        request.setAttribute("spelersbelgie",databankenPlayers.playersFromBelgium());
+
+        return "lijst.jsp";
+    }
+
+    private String delete (HttpServletRequest request, HttpServletResponse response){
+        System.out.println(request.getParameter("deleted"));
+        Player player = databankenPlayers.findPlayer((String)request.getParameter("deleted"));
+        ArrayList arrayList = databankenPlayers.getSpelers();
+        arrayList.remove(player);
+        request.setAttribute("p1","null");
+        request.setAttribute("p2","null");
+        request.setAttribute("db",arrayList);
+        request.setAttribute("spelersbelgie",databankenPlayers.playersFromBelgium());
+
+        return "lijst.jsp";
+    }
+
+    private String add (HttpServletRequest request, HttpServletResponse response){
+        String naam = request.getParameter("naam");
+        String nationaliteit = request.getParameter("nationaliteit");
+        String team = request.getParameter("team");
+
+        Player player = new Player(naam,nationaliteit,team);
+        databankenPlayers.addPlayer(player);
+        request.setAttribute("db",databankenPlayers.getSpelers());
+        return "lijst.jsp";
+    }
+
+    private String find(HttpServletRequest request, HttpServletResponse response){
+        String naam = request.getParameter("naam");
+        String p1 = "de speler dat u zoekt : ";
+        String p2 = "bestaat niet ";
+        for (int i = 0; i < databankenPlayers.getSpelers().size(); i++){
+            if (databankenPlayers.getSpelers().get(i).getNaam().equals(naam)){
+                p2 = "is " + naam + " en heeft als land van afkomst "+ databankenPlayers.getSpelers().get(i).getNationaliteit() + " en zit in " + databankenPlayers.getSpelers().get(i).getTeam();
+            }
+        }
+        request.setAttribute("p1" , p1);
+        request.setAttribute("p2" , p2);
+        request.setAttribute("db",databankenPlayers.getSpelers());
+        request.setAttribute("spelersbelgie",databankenPlayers.playersFromBelgium());
+        return "lijst.jsp";
+    }
+
+}
---
+++ web/lijst.jsp
@@ -0,0 +1,64 @@
+<%@ page import="domain.database.DatabankenPlayers" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="domain.model.Player" %><%--
+  Created by IntelliJ IDEA.
+  User: PC001
+  Date: 10-10-2020
+  Time: 00:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>player database</title>
+</head>
+<body class="body">
+<div class="bg"></div>
+<header>
+    <h1>DATABASE</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="index.html" >HOME</a></li>
+        <li><a href="tweedePag.html">ADD PLAYER</a></li>
+        <li><a href="servlet?task=overview" class="kleur">PLAYER LIST</a></li>
+    </ul>
+</nav>
+<article>
+    <h2> DATABASE WORLDS</h2>
+    <a class="find" href="find.jsp">find a player by name</a>
+    <%  String a = (String) request.getAttribute("p1");
+        String b = (String) request.getAttribute("p2");
+
+        if (a.equals("null") && b.equals("null")){
+            a = " ";
+            b = " ";
+        }
+    %>
+    <p><%=a%></p>
+    <p><%=b%></p>
+    <table class="tabel">
+        <thead>
+        <th>naam</th>
+        <th>land van afkomst</th>
+        <th>team</th>
+        </thead>
+        <%!ArrayList<Player> spelers = new ArrayList<>();%>
+        <%spelers =  (ArrayList<Player>)request.getAttribute("db");%>
+        <%for (int i = 0; i < spelers.size(); i++){%>
+            <tr>
+                <td><p><%=spelers.get(i).getNaam()%></p></td>
+                <td><p><%=spelers.get(i).getNationaliteit()%></p></td>
+                <td><p><%=spelers.get(i).getTeam()%></p></td>
+                <td><a href="servlet?task=delete&deleted=<%=spelers.get(i).getNaam()%>">delete</a></td>
+            </tr>
+        <%}%>
+    </table>
+    <p>how many players from belgium = <%=request.getAttribute("spelersbelgie")%></p>
+</article>
+</body>
+</html>
